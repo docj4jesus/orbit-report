3edda1c903b31562a8a118381f653ec4bcb88a45
Janell Renee Johnson <docj4jesus@gmail.com>
2020-09-24 16:47:31 -0500

    Assignment #6 Complete Minus Bonus Mission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,11 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
+  displayList: Satellite[];
+
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
@@ -28,10 +31,25 @@ export class AppComponent {
             this.sourceList.push(satellite);
             i++;
           }
+          
+          this.displayList = this.sourceList.slice(0);
 
        }.bind(this));
     }.bind(this));
 
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
  }
 
